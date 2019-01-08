a687a66a0c0234728d0c5538dcec2c66f654a5a9
Wajeeh <wajeehmisbahkhan@yahoo.com>
2019-01-08 14:17:29 +0500

    Basic structure ready - Recording video stream and screen sharing

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
-var mediaRecorders = {video, screen};
-var chunks = {video: [], screen: []};
 //Audio & Video
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(function (stream) {
+// navigator.mediaDevices.getUserMedia({
+//     video: true,
+//     audio: true
+// }).then(function (stream) {
+//     displayVideo(stream);
+//     //Record
+//     $("#video-record").prop('disabled', false).click(function (e) {recordVideo(stream)});
+// }).catch(function (err) {
+//     document.writeln(err);
+// });
+
+//Screen
+getScreen().then(function (stream) {
     displayVideo(stream);
+    //Record
     $("#video-record").prop('disabled', false).click(function (e) {recordVideo(stream)}); //Closure
 }).catch(function (err) {
     document.writeln(err);
 });
 
+function getScreen () {
+    if (navigator.getDisplayMedia) {
+        return navigator.getDisplayMedia({video: true, audio: true});
+    } else if (navigator.mediaDevices.getDisplayMedia) {
+        return navigator.mediaDevices.getDisplayMedia({video: true, audio: true});
+    } else {
+        return navigator.mediaDevices.getUserMedia({video: {mediaSource: 'screen'}, audio: true});
+    }
+}
+
+//All Utility Functions
 function displayVideo (stream) {
     var video = document.getElementById("video");
     video.srcObject = stream;
@@ -19,98 +38,45 @@ function displayVideo (stream) {
 
 function recordVideo (stream) {
     var options = {type: 'video/webm'};
-    mediaRecorders.video = new MediaRecorder(stream, options);
-    mediaRecorders.video.ondataavailable = function(e) {
-        chunks.video.push(e.data);
+    var chunks = [];
+    var mediaRecorder = new MediaRecorder(stream, options);
+    mediaRecorder.ondataavailable = function(e) {
+        chunks.push(e.data);
     }
-    mediaRecorders.video.start();
+    mediaRecorder.start();
     $("#video-record").prop('disabled', true);
-    $("#video-record-stop").prop('disabled', false).click(function (e) {stopRecording()});
+    $("#video-record-stop").prop('disabled', false).click(function (e) {stopRecording(mediaRecorder, chunks);});
 }
 
-function stopRecording () {
+function stopRecording (mediaRecorder, chunks) {
     $("#video-record").prop('disabled', false);
     $("#video-record-stop").prop('disabled', true);
-    mediaRecorders.video.onstop = function (e) {
+    mediaRecorder.onstop = function (e) {
 
-        // var clipContainer = document.createElement('article');
-        // var clipLabel = document.createElement('p');
-        // var audio = document.createElement('audio');
-        // var deleteButton = document.createElement('button');
+        var clipContainer = document.createElement('article');
+        var clipLabel = document.createElement('p');
+        var video = document.createElement('video');
+        var deleteButton = document.createElement('button');
                 
-        // clipContainer.classList.add('clip');
-        // audio.setAttribute('controls', '');
-        // deleteButton.innerHTML = "Delete";
-        // clipLabel.innerHTML = "Produced Audio";
+        clipContainer.classList.add('clip');
+        video.setAttribute('controls', '');
+        deleteButton.innerHTML = "Delete";
+        clipLabel.innerHTML = "Produced Video";
 
-        // clipContainer.appendChild(audio);
-        // clipContainer.appendChild(clipLabel);
-        // clipContainer.appendChild(deleteButton);
-        // document.body.appendChild(clipContainer);
+        clipContainer.appendChild(video);
+        clipContainer.appendChild(clipLabel);
+        clipContainer.appendChild(deleteButton);
+        document.body.appendChild(clipContainer);
 
+        var superBuffer = new Blob(chunks);
+        chunks = []; //Empty
+        video.src = window.URL.createObjectURL(superBuffer);
 
-        // var blob = new Blob(chunks.video, { 'type' : 'audio/ogg; codecs=opus' });
-        // chunks.video = []; //Empty
-        // var audioURL = window.URL.createObjectURL(blob);
-        // audio.src = audioURL;
+        deleteButton.onclick = function(e) {
+            var evtTgt = e.target;
+            evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+        }
 
-        // deleteButton.onclick = function(e) {
-        //     var evtTgt = e.target;
-        //     evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-        // }
-        var videoElement = document.createElement('video');
-        var superBuffer = new Blob(chunks.video);
-        videoElement.src = window.URL.createObjectURL(superBuffer);
-        document.body.appendChild(videoElement);
-        
-        // var clipContainer = document.createElement('article');
-        // var clipLabel = document.createElement('p');
-        // var audio = document.createElement('audio');
-        // var deleteButton = document.createElement('button');
-                
-        // clipContainer.classList.add('clip');
-        // audio.setAttribute('controls', '');
-        // deleteButton.innerHTML = "Delete";
-        // clipLabel.innerHTML = "Produced Audio";
-
-        // clipContainer.appendChild(audio);
-        // clipContainer.appendChild(clipLabel);
-        // clipContainer.appendChild(deleteButton);
-        // document.body.appendChild(clipContainer);
-
-
-        // var blob = new Blob(chunks.video, { 'type' : 'audio/ogg; codecs=opus' });
-        // chunks.video = []; //Empty
-        // var audioURL = window.URL.createObjectURL(blob);
-        // audio.src = audioURL;
-
-        // deleteButton.onclick = function(e) {
-        //     var evtTgt = e.target;
-        //     evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-        // }
     }
-    mediaRecorders.video.stop();
-    mediaRecorders = null;
+    mediaRecorder.stop();
 }
-
-//Screen
-/*getScreen().then(function (stream) {
-    var video = document.createElement('video');
-    document.body.appendChild(video);
-    video.srcObject = stream;
-    video.play();
-}).catch(function (err) {
-    document.writeln(err);
-});
-
-function getScreen () {
-    if (navigator.getDisplayMedia) {
-        console.log();
-        return navigator.getDisplayMedia({video: true});
-    } else if (navigator.mediaDevices.getDisplayMedia) {
-        return navigator.mediaDevices.getDisplayMedia({video: true});
-    } else {
-        console.log("Video");
-        return navigator.mediaDevices.getUserMedia({video: {mediaSource: 'screen'}});
-    }
-}*/
